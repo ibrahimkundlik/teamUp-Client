a570060f4f21c69d93f523bafea97f18551b2b5d
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-06-04 08:53:36 +0530

    Added auth header for axios requests

--- src/api/api-call.js
+++ src/api/api-call.js
@@ -4,5 +4,14 @@ const API = axios.create({
 	baseURL: "http://localhost:5000",
 });
 
+API.interceptors.request.use((req) => {
+	if (localStorage.getItem("profile")) {
+		req.headers.authorization = `Bearer ${
+			JSON.parse(localStorage.getItem("profile")).token
+		}`;
+	}
+	return req;
+});
+
 export const login = (formData) => API.post(`/users/login`, formData);
 export const signup = (formData) => API.post(`/users/signup`, formData);
