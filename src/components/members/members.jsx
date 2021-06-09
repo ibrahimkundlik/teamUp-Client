60e3144c737be1d45a4fcb0ab8c436e0b2fd500e
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-06-09 12:20:04 +0530

    Added styling to ExistUserTeams and Members component for mobile layout

--- src/components/members/members.jsx
+++ src/components/members/members.jsx
@@ -4,6 +4,7 @@ import "./members.scss";
 const Members = ({ members }) => {
 	return (
 		<div className="members-cont">
+			<h5>Members:</h5>
 			{members.slice(0, 5).map((member) => (
 				<div className="member" key={member._id?._id}>
 					<p>
--- src/components/teams/exist-user-teams/exist-user-teams.jsx
+++ src/components/teams/exist-user-teams/exist-user-teams.jsx
@@ -11,37 +11,41 @@ import Members from "../../members/members";
 const ExistUserTeams = ({ teams, username }) => {
 	return (
 		<div className="exist-user-teams-cont">
-			<h2>
+			<h2 className="exist-user-head">
 				Hi {username.split(" ")[0]}{" "}
 				<span role="img" aria-label="waving">
 					👋
 				</span>
 			</h2>
 			<div className="team-actions">
-				<h4>Team actions:</h4>
+				<h4 className="disp-sm team-actions-head">Team actions:</h4>
 				<CustomButton className="create-btn">
 					<AiOutlinePlusCircle />
-					<h4>Create new team</h4>
+					<h4 className="disp-lg">Create new team</h4>
+					<h4 className="disp-sm">New</h4>
 				</CustomButton>
 				<CustomButton className="join-btn">
 					<AiOutlineAppstoreAdd />
-					<h4>Join existing teams</h4>
+					<h4 className="disp-lg">Join existing teams</h4>
+					<h4 className="disp-sm">Join</h4>
 				</CustomButton>
 				<CustomButton className="request-btn">
 					<AiOutlineMergeCells />
-					<h4>Team requests</h4>
+					<h4 className="disp-lg">Team requests</h4>
+					<h4 className="disp-sm">Requests</h4>
 				</CustomButton>
 			</div>
 			<div className="current-teams">
-				<h4>Your current teams:</h4>
+				<h4 className="team-actions-head">Your current teams:</h4>
 				<ul className="teams-list">
 					{teams.map((team) => {
 						const { name, description, members, _id } = team;
 						return (
 							<li key={_id} className="team">
 								<h3>{name}</h3>
-								<p>{description}</p>
+								<p className="team-description">{description}</p>
 								<Members members={members} />
+								<CustomButton>Dashboard</CustomButton>
 							</li>
 						);
 					})}
