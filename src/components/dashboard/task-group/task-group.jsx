3e0dfe06f959d0da69ad0ae4f386939c4df0b255
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-06-28 02:47:56 +0530

    Worked on styling for task-group and task component

--- src/components/dashboard/task-group/task-group.jsx
+++ src/components/dashboard/task-group/task-group.jsx
@@ -1,12 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { seperateTasksAction } from "../../../redux/task/task.action";
 import Task from "../task/task";
 import "./task-group.scss";
+import { TiArrowMaximise, TiArrowMinimise } from "react-icons/ti";
+
+const INITIAL_STATE = {
+	backlog: false,
+	progress: false,
+	review: false,
+	done: false,
+};
 
 const TaskGroup = ({ tasks }) => {
 	const dispatch = useDispatch();
 	const seperatedTasks = useSelector((state) => state.task.tasks);
+	const [hideState, setHideState] = useState(INITIAL_STATE);
 
 	useEffect(() => {
 		dispatch(seperateTasksAction(tasks));
@@ -16,12 +25,26 @@ const TaskGroup = ({ tasks }) => {
 		<div className="task-group-cont">
 			{seperatedTasks &&
 				Object.entries(seperatedTasks).map(([key, value]) => (
-					<div className="task-group" key={key}>
-						<h3 className="task-group-head">{key}</h3>
+					<div className={`task-group ${key}`} key={key}>
+						<h3 className="task-group-head">
+							{key === "progress" || key === "review" ? `in ${key}` : key}
+						</h3>
 						<p className="task-group-count">{value.length}</p>
-						{value.map((task) => (
-							<Task task={task} key={task._id} />
-						))}
+						<div
+							className="arrow-icon"
+							onClick={() => {
+								setHideState({ ...hideState, [key]: !hideState[key] });
+							}}
+						>
+							{hideState[key] ? <TiArrowMaximise /> : <TiArrowMinimise />}
+						</div>
+						<div
+							className={`${hideState[key] ? "hide-group" : ""} task-category`}
+						>
+							{value.map((task) => (
+								<Task task={task} key={task._id} />
+							))}
+						</div>
 					</div>
 				))}
 		</div>
--- src/components/dashboard/task/task.jsx
+++ src/components/dashboard/task/task.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./task.scss";
+import { TiAttachment } from "react-icons/ti";
 
 const Task = ({ task }) => {
 	return (
 		<div className="task-main-cont">
-			<h4 className="task-name">{task.name}</h4>
-			<p className="task-attachments">{task.attachments.length}</p>
-			<p className="task-priority">{task.priority}</p>
+			<h4 className="task-name">
+				{task.name[0].toUpperCase() + task.name.substring(1)}
+			</h4>
+			<div className="task-attachments">
+				<TiAttachment />
+				<p>{task.attachments.length}</p>
+			</div>
+			<p className={`task-priority ${task.priority}-clr`}>{task.priority}</p>
 		</div>
 	);
 };
