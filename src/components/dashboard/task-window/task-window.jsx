0067cc2d895a113f46d3af275732d1490e8caf77
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-06-29 02:01:57 +0530

    Added styling for TaskWindow component

--- src/components/dashboard/task-window/task-window.jsx
+++ src/components/dashboard/task-window/task-window.jsx
@@ -3,23 +3,29 @@ import "./task-window.scss";
 import { RiDashboardLine, RiCloseCircleLine } from "react-icons/ri";
 import Members from "../../members/members";
 
-const TaskWindow = ({ handleCloseForm, task, teamName, members }) => {
+const TaskWindow = ({ handleCloseForm, task, teamName }) => {
 	return (
 		<div className="task-window-cont">
 			<div className="task-nav">
-				<RiDashboardLine />
+				<RiDashboardLine className="dashboard-icon" />
 				<h3 className="team-name">{teamName} Board</h3>
-				<RiCloseCircleLine onClick={() => handleCloseForm()} />
+				<RiCloseCircleLine
+					className="close-icon"
+					onClick={() => handleCloseForm()}
+				/>
 			</div>
+			<h3 className="task-name">
+				{task.name[0].toUpperCase() + task.name.substring(1)}
+			</h3>
 			<div className="task-type-priority">
-				<h4 className="task-type">{task.type}</h4>
+				<h4 className="task-type">
+					{task.type === "progress" || task.type === "review"
+						? `in ${task.type}`
+						: task.type}
+				</h4>
 				<h4 className={`task-priority ${task.priority}-clr`}>
 					{task.priority}
 				</h4>
-			</div>
-			<div className="task-name-cont">
-				<h5>Task:</h5>
-				<h2 className="task-name">{task.name}</h2>
 			</div>
 			<Members members={task.assigned} />
 			<div className="task-description-cont">
